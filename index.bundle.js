4734ba99d5477697453492955df0555af511fd8c
dominik-kiss <dominik.kiss997@gmail.com>
2023-03-17 13:08:36 +0100

    Basic npm and webpack setup

---
+++ index.bundle.js
@@ -0,0 +1,20 @@
+(self["webpackChunksite_project"] = self["webpackChunksite_project"] || []).push([["index"],{
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/***/ (() => {
+
+
+console.log("Hello World!");
+
+/***/ })
+
+},
+/******/ __webpack_require__ => { // webpackRuntimeModules
+/******/ var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+/******/ var __webpack_exports__ = (__webpack_exec__("./src/index.js"));
+/******/ }
+]);
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguYnVuZGxlLmpzIiwibWFwcGluZ3MiOiI7Ozs7Ozs7O0FBQUE7QUFDQSIsInNvdXJjZXMiOlsid2VicGFjazovL3NpdGUtcHJvamVjdC8uL3NyYy9pbmRleC5qcyJdLCJzb3VyY2VzQ29udGVudCI6WyJcclxuY29uc29sZS5sb2coXCJIZWxsbyBXb3JsZCFcIik7Il0sIm5hbWVzIjpbXSwic291cmNlUm9vdCI6IiJ9
