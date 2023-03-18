2246ae927546649914ce836e30528089b4bffcfa
dominik-kiss <dominik.kiss997@gmail.com>
2023-03-18 16:37:34 +0100

    Simplify sitecontrol function and remove unused import

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import './style.css';
 import homePage from "./pageContent";
-import aboutUsPage from './aboutUs';
 import { setStartingPosition, moveCarousel } from './carousel';
 
 const firstLoad = (() => {
@@ -9,41 +8,34 @@ const firstLoad = (() => {
   firstDiv.classList.add("centered-flex");
   document.body.appendChild(firstDiv);
 
+  homePage();
+  const slides = document.querySelectorAll(".slide");
+  setStartingPosition(slides);
+  moveCarousel(slides, 7000);
+
   siteControl();
 })();
 
-function siteControl(tab) {
-  switch (tab) {
-    case "about-us":
-      aboutUsPage();
-      break;
-    case "menu":
-      break;
-    case "contact-us":
-    case "reservation-button":
-      break;
-    default:
-      homePage();
-      const slides = document.querySelectorAll(".slide");
-      setStartingPosition(slides);
-      moveCarousel(slides, 7000);
-  }
-  
-  handleTabSwitch();
-};
-
-function handleTabSwitch() {
-  // Set up event listeners for the site navigation tabs
+function siteControl() {
   const naviTabs = document.querySelectorAll(".navi");
+
   naviTabs.forEach(tab => tab.addEventListener("click", (event) => {
-  // When one of the tabs is clicked...
-    // Store the id of the clicked tab for later reference
-    let tabClicked = event.target.id;
-
-    // Clear all child nodes from under the "content" div
-    document.getElementById("content").textContent = "";
-
-    // Populate the page with the HTML content and styles of the tab that was clicked
-    siteControl(tabClicked);
+    console.log(event.target.id);
+    switch (event.target.id) {
+      case "about-us":
+        window.scrollTo({ top: 940, behavior: 'smooth' });
+        break;
+      case "menu":
+        window.scrollTo({ top: 1750, behavior: 'smooth' });
+        break;
+      case "gallery":
+        window.scrollTo({ top: 2050, behavior: 'smooth' });
+        break;
+      case "reservation-button":
+      case "contact-us":
+        window.scrollTo({ top: 2150, behavior: 'smooth' });
+        break;
+    }
   }));
-}
+
+};
