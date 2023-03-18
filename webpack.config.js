4919ce57d7b9647544643bba386163dc871869c3
dominik-kiss <dominik.kiss997@gmail.com>
2023-03-18 19:42:00 +0100

    Turn off performance hints, change mode to production, update page title

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 // const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer");
 
 module.exports = {
-    mode: "development",
+    mode: "production",
     entry: {
         index: "./src/index.js",
     },
@@ -41,10 +41,11 @@ module.exports = {
     plugins: [
        // new BundleAnalyzerPlugin(),
        new HtmlWebpackPlugin({
-        title: 'Development',
+        title: 'King Hill Restaurant',
       }),
     ],
     optimization: {
       runtimeChunk: 'single',
     },
+    performance: { hints: false }
 };
